36995224a78b73991cf89f74c5ba0920f8f49eb6
Heron Yang <v6510young88@gmail.com>
2014-10-10 23:52:39 +0800

    phrase refine

--- index.php
+++ index.php
@@ -43,70 +43,133 @@
         </div>
 
         <div class="content-wrapper">
-            <div class="content is-center">
-                <h2 class="content-head">配對成功名單</h2>
-                <h3>每日晚間09:09放榜</h3>
-                <p>Hello</p>
+            <div class="content">
+                <div class="is-center">
+                    <h2 class="content-head">告白成功名單</h2>
+                    <h3>每日晚間09:09放榜</h3>
+                </div>
+                <div class="pure-g">
+                    <div class="pure-u-1-6"></div>
+                    <div class="pure-u-2-3">
+                        <hr/>
+                        <h4>2014.04.21 星期日</h4>
+                        <div class="gb-success-single">
+                            <h5><a href="http://fb.com/apple" target="_blank">周杰倫</a> 跟 <a href="http://fb.com/banana" target="_blank">昆凌</a> 在一起了！</h5>
+                            <div class="pure-g">
+                                <div class="pure-u-1-2">
+                                    <div class="l-box">
+                                        <p><b>周杰倫</b>: 有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!</p>
+                                        <p><i><time datetime="2001-05-15 19:00">13:15</time></i></p>
+                                    </div>
+                                </div>
+                                <div class="pure-u-1-2">
+                                    <div class="l-box">
+                                        <p><b>昆凌</b>: 有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="gb-success-single">
+                            <h5><a href="http://fb.com/apple" target="_blank">周杰倫</a> 跟 <a href="http://fb.com/banana" target="_blank">昆凌</a> 在一起了！</h5>
+                            <div class="pure-g">
+                                <div class="pure-u-1-2">
+                                    <div class="l-box">
+                                        <p><b>周杰倫</b>: 有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!</p>
+                                    </div>
+                                </div>
+                                <div class="pure-u-1-2">
+                                    <div class="l-box">
+                                        <p><b>昆凌</b>: 有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="gb-success-single">
+                            <h5><a href="http://fb.com/apple" target="_blank">周杰倫</a> 跟 <a href="http://fb.com/banana" target="_blank">昆凌</a> 在一起了！</h5>
+                            <div class="pure-g">
+                                <div class="pure-u-1-2">
+                                    <div class="l-box">
+                                        <p><b>周杰倫</b>: 有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!</p>
+                                    </div>
+                                </div>
+                                <div class="pure-u-1-2">
+                                    <div class="l-box">
+                                        <p><b>昆凌</b>: 有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pure-u-1-6"></div>
+                </div>
+
+
+                <div class="pure-g">
+                    <div class="pure-u-1-6"></div>
+                    <div class="pure-u-2-3">
+                        <hr/>
+                        <h4>2014.04.20 星期六</h4>
+                        <div class="gb-success-single">
+                            <h5><a href="http://fb.com/apple" target="_blank">周杰倫</a> 跟 <a href="http://fb.com/banana" target="_blank">昆凌</a> 在一起了！</h5>
+                            <div class="pure-g">
+                                <div class="pure-u-1-2">
+                                    <div class="l-box">
+                                        <p><b>周杰倫</b>: 有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!</p>
+                                    </div>
+                                </div>
+                                <div class="pure-u-1-2">
+                                    <div class="l-box">
+                                        <p><b>昆凌</b>: 有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="gb-success-single">
+                            <h5><a href="http://fb.com/apple" target="_blank">周杰倫</a> 跟 <a href="http://fb.com/banana" target="_blank">昆凌</a> 在一起了！</h5>
+                            <div class="pure-g">
+                                <div class="pure-u-1-2">
+                                    <div class="l-box">
+                                        <p><b>周杰倫</b>: 有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!</p>
+                                    </div>
+                                </div>
+                                <div class="pure-u-1-2">
+                                    <div class="l-box">
+                                        <p><b>昆凌</b>: 有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="gb-success-single">
+                            <h5><a href="http://fb.com/apple" target="_blank">周杰倫</a> 跟 <a href="http://fb.com/banana" target="_blank">昆凌</a> 在一起了！</h5>
+                            <div class="pure-g">
+                                <div class="pure-u-1-2">
+                                    <div class="l-box">
+                                        <p><b>周杰倫</b>: 有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!</p>
+                                    </div>
+                                </div>
+                                <div class="pure-u-1-2">
+                                    <div class="l-box">
+                                        <p><b>昆凌</b>: 有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!有你真好!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pure-u-1-6"></div>
+                </div>
 
             </div>
 
             <div class="ribbon l-box-lrg pure-g">
                 <div class="l-box-lrg is-center pure-u-1 pure-u-md-1-2 pure-u-lg-2-5">
-                    <img class="pure-img-responsive" alt="File Icons" width="300" src="img/common/file-icons.png">
+                    <img class="pure-img-responsive" alt="File Icons" width="128" src="img/common/anger.png">
                 </div>
                 <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-3-5">
 
-                    <h2 class="content-head content-head-ribbon">Laboris nisi ut aliquip.</h2>
+                    <h2 class="content-head content-head-ribbon">你想後悔嗎？</h2>
+                    <q>五月天：「有些事現在不做一輩子都不會做了」</q>
 
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor.
-                    </p>
                 </div>
-            </div>
-
-            <div class="content">
-                <h2 class="content-head is-center">Dolore magna aliqua. Uis aute irure.</h2>
-
-                <div class="pure-g">
-                    <div class="l-box-lrg pure-u-1 pure-u-md-2-5">
-                        <form class="pure-form pure-form-stacked">
-                            <fieldset>
-
-                                <label for="name">Your Name</label>
-                                <input id="name" type="text" placeholder="Your Name">
-
-
-                                <label for="email">Your Email</label>
-                                <input id="email" type="email" placeholder="Your Email">
-
-                                <label for="password">Your Password</label>
-                                <input id="password" type="password" placeholder="Your Password">
-
-                                <button type="submit" class="pure-button">Sign Up</button>
-                            </fieldset>
-                        </form>
-                    </div>
-
-                    <div class="l-box-lrg pure-u-1 pure-u-md-3-5">
-                        <h4>Contact Us</h4>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                            consequat.
-                        </p>
-
-                        <h4>More Information</h4>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua.
-                        </p>
-                    </div>
-                </div>
-
             </div>
 
             <div class="footer l-box is-center">
