78bbcff70cc179282fbc6892e67ad1e65267f923
Heron Yang <heron.yang.tw@gmail.com>
2014-10-26 01:12:27 +0800

    update error.php

--- error.php
+++ error.php
@@ -1,3 +1,25 @@
 <?php
-echo "Sorry... Error is here";
+$title = '告白神器';
+$description = '向一位FB好友告白，只有他也向你告白時才會被知道！';
 ?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
+        <meta name="description" content="<?php echo $description; ?>">
+        <title><?php echo $title; ?></title>
+    </head>
+    <body>
+    <style>
+html { 
+  background: url(img/404.jpg) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+    </style>
+    </body>
+</html>
